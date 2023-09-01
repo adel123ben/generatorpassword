4bdbe691b16f0dd56245a976bf14d8e907ec6c8f
adel123ben <103518966+adel123ben@users.noreply.github.com>
2023-09-02 00:58:56 +0200

    Update Header.tsx

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,12 +1,21 @@
+"use client";
+
 import Image from "next/image"
 
 
+
+
 const Header = () => {
+
+   
     return(
         <div className="flex flex-row p-11 justify-between">
             <img className="w-11 h-11 cursor-pointer" src="https://smiletutor.sg/wp-content/uploads/2020/10/password.png" alt="" />
             <div>
-                <button className="border-0 rounded-full bg-neutral-950 text-white text-sm py-2 px-5">Github</button>
+                <a href="https://github.com/adel123ben/generatorpassword">
+                <button   className="border-0 rounded-full bg-neutral-950 text-white text-sm py-2 px-5">Github</button>
+                </a>
+                
             </div>
         </div>
     )
